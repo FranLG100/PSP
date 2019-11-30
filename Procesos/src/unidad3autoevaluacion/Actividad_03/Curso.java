b848ef05e14850c4b071cd456e4d9c9e41bef0cd
Fran <fralg100@gmail.com>
2019-11-30 16:18:12 +0100

    Ejercicio Autoevaluacion 3

---
+++ Procesos/src/unidad3autoevaluacion/Actividad_03/Curso.java
@@ -0,0 +1,37 @@
+package unidad3autoevaluacion.Actividad_03;
+
+import java.io.Serializable;
+
+public class Curso implements Serializable{
+
+	String id;
+	String descripcion;
+	
+	public Curso(String id, String descripcion) {
+		super();
+		this.id = id;
+		this.descripcion = descripcion;
+	}
+
+	public Curso() {
+		super();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	
+}
