b8d1fd5d1ba9434131d6441599ff4edaffb3c213
fralg100@gmail.com <fralg100@gmail.com>
2019-11-30 10:24:03 +0100

    Ejemplo adivina numero

---
+++ Procesos/src/ejemplostema3parte2/JugadorAdivina.java
@@ -0,0 +1,74 @@
+package ejemplostema3parte2;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+public class JugadorAdivina {
+
+	public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+		String host="localhost";
+		int puerto=6001;
+		Socket cliente=new Socket(host,puerto);
+		
+		ObjectOutputStream fsalida= new ObjectOutputStream(cliente.getOutputStream());
+		ObjectInputStream fentrada= new ObjectInputStream(cliente.getInputStream());
+		
+		Scanner sc=new Scanner(System.in);
+		String cadena="";
+		
+		Datos datos=(Datos) fentrada.readObject();
+		int identificador=datos.getIdentificador();
+		System.out.println("Id jugador: "+identificador);
+		System.out.println(datos.getCadena());
+		
+		if(!datos.isJuega())
+			cadena="*";
+		
+		while(datos.isJuega() && !cadena.trim().equals("*")) {
+			System.out.println("Intento: "+(datos.getIntentos()+1)+" =>Introduce numero: ");
+			cadena=sc.nextLine();
+			Datos d=new Datos();
+			
+			if(!validarCadena(cadena)) continue;
+			
+			d.setCadena(cadena);
+			
+			fsalida.reset();
+			fsalida.writeObject(d);
+			
+			datos=(Datos) fentrada.readObject();
+			System.out.println("\t"+datos.getCadena());
+			
+			if(datos.getIntentos()>=5) {
+				System.out.println("\t<<JUEGO FINALIZADO, NO HAY MÁS INTENTOS>>");
+				cadena="*";
+			}
+			if(datos.isGana()) {
+				System.out.println("<<HAS GANADO!! EL JUEGO HA TERMINADO>>");
+				cadena="*";
+			}else
+				if(!(datos.isJuega())) {
+					System.out.println("<<EL JUEGO HA TERMINADO, HAN ADIVINADO EL NUMERO>>");
+					cadena="*";
+				}
+		}
+		
+		fsalida.close();
+		fentrada.close();
+		System.out.println("Fin de proceso...");
+		sc.close();
+		cliente.close();
+	}
+	
+	private static boolean validarCadena(String cadena) {
+		boolean valor=false;
+		try {
+			Integer.parseInt(cadena);
+			valor=true;
+		}catch (NumberFormatException e) {
+			// TODO: handle exception
+		}
+		return valor;
+	}
+
+}
---
+++ Procesos/src/ejemplostema3parte2/ServidorAdivina.java
@@ -0,0 +1,27 @@
+package ejemplostema3parte2;
+import java.io.*;
+import java.net.*;
+public class ServidorAdivina {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		ServerSocket servidor=new ServerSocket(6001);
+		System.out.println("Servidor iniciado...");
+		
+		int numero=(int)(1+25*Math.random());
+		System.out.println("NÚMERO A ADIVINAR=> "+numero);
+		
+		ObjetoCompartido objeto=new ObjetoCompartido(numero);
+		int id=0;
+		
+		while(true) {
+			Socket cliente= new Socket();
+			cliente=servidor.accept();
+			id++;
+			HiloServidorAdivina hilo=new HiloServidorAdivina(cliente, objeto, id);
+			hilo.start();
+		}
+	}
+
+}
