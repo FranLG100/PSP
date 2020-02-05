cfdd695db014a040995c25d98c1b6e4037b342d2
Fran <fralg100@gmail.com>
2020-02-05 19:19:39 +0100

    Ejercicios para entregar

---
+++ Procesos/src/ejerciciostema5/Ejemplo5.java
@@ -0,0 +1,42 @@
+package ejerciciostema5;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Ejemplo5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try {
+			FileOutputStream fileout=new FileOutputStream("DATOS.dat");
+			ObjectOutputStream dataOS= new ObjectOutputStream(fileout);
+			
+			MessageDigest md=MessageDigest.getInstance("SHA-256");
+			String datos="En un lugar de la Mancha de cuyo nombre no quiero acordarme"+
+			",no ha mucho tiempo que vivia un hidalgo de los de lanza en astillero"+
+					",adarga antigua, rocin flaco y galgo corredor";
+			
+			byte[] dataBytes=datos.getBytes();
+			
+			md.update(dataBytes);
+			byte resumen[]=md.digest();
+			dataOS.writeObject(datos);
+			dataOS.writeObject(resumen);
+			
+			dataOS.close();
+			fileout.close();
+			
+		} catch (IOException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}catch (NoSuchAlgorithmException e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ Procesos/src/ejerciciostema5/Ejemplo6.java
@@ -0,0 +1,42 @@
+package ejerciciostema5;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.security.MessageDigest;
+
+public class Ejemplo6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try {
+			FileInputStream fileout =new FileInputStream("DATOS.dat");
+			ObjectInputStream dataOS=new ObjectInputStream(fileout);
+			Object o=dataOS.readObject();
+			
+			String datos=(String) o;
+			System.out.println("Datos: "+datos);
+			
+			o=dataOS.readObject();
+			byte resumenOriginal[]=(byte[]) o;
+			
+			MessageDigest md=MessageDigest.getInstance("SHA-256");
+			
+			md.update(datos.getBytes());
+			byte resumenActual[]=md.digest();
+			
+			if(MessageDigest.isEqual(resumenActual, resumenOriginal))
+				System.out.println("DATOS VALIDOS");
+			else
+				System.out.println("DATOS NO VALIDOS");
+			
+			dataOS.close();
+			fileout.close();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+
+}
